9e8f2ba680fe2c8ab6bf1796fa3bca99c2e0deaa
IleaneBB <a2020110849@estudiantes.upsa.edu.bo>
2021-10-12 22:53:54 -0400

    Agregar archivos de proyecto.

---
+++ CancionesWeb/Models/Canciones.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CancionesWeb.Models
+{
+    public class Canciones
+    {
+        [Key]
+        public string CancionID { get; set; }
+       // [Required]
+       // [Display(Name="Nombre de la canción")]
+        //public string Name { get; set; }
+        [Display(Name = "Autor Principal")]
+        public string Author { get; set; }
+        [Display(Name = "Letra de la canción")]
+        public string Lyrics { get; set; }
+        [Display(Name = "Enlace de youtube")]
+        public string URL { get; set; }
+
+    }
+}
